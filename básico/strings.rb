4b1df95ead23a8047b3b70cd854c7183abb14bd1
ArthurBernardoDev <arthurutschgg@gmail.com>
2021-11-07 17:18:00 -0300

    strings

---
+++ básico/strings.rb
@@ -0,0 +1,7 @@
+# existem duas formas de definir string em ruby
+# "" ou '', sendo que com "" podemos fazer interpolação
+# interpolação = colocar variavel dentro de uma string
+
+nome = "Arthur"
+mensagem = "Bem vindo #{nome}"
+puts mensagem
